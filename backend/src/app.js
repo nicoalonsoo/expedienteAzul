40b6e65428cea856bc1cf51c7dbb350c926dc0a2
NAlonsoBulacio <nicoalonso99.na@gmail.com>
2023-09-28 13:04:17 -0300

    prueba 2

--- backend/src/app.js
+++ backend/src/app.js
@@ -20,12 +20,14 @@ server.use((req, res, next) => {
     'https://expedienteazul.vercel.app',
     'http://localhost:3000',
     'https://expedienteazul-pw32.vercel.app',
+    // Agrega más URLs permitidas aquí según sea necesario
   ];
-
+  
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
+  
   
   res.header('Access-Control-Allow-Origin', allowedOrigins.join(', '));
   res.header('Access-Control-Allow-Credentials', 'true');
