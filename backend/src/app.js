0358be0d785a67f7d0f7cd7685feb681d980ffde
NAlonsoBulacio <nicoalonso99.na@gmail.com>
2023-09-28 13:06:37 -0300

    aver

--- backend/src/app.js
+++ backend/src/app.js
@@ -27,9 +27,6 @@ server.use((req, res, next) => {
   if (allowedOrigins.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
-  
-  
-  res.header('Access-Control-Allow-Origin', allowedOrigins.join(', '));
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
