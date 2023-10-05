909e09d6f9623872898c24d8c54d263b592dc101
NAlonsoBulacio <nicoalonso99.na@gmail.com>
2023-10-05 13:32:50 -0300

    estilos landing video

--- frontend/src/views/LandingVideo/LandingVideo.jsx
+++ frontend/src/views/LandingVideo/LandingVideo.jsx
@@ -4,6 +4,7 @@ import DropboxVideo from "../../componentes/Video/Video";
 import Header from "../../componentes/Header/Header";
 import Footer from "../../componentes/Footer/Footer";
 // import fondo from '../../multimedia/fondo.jpg'
+import click from "../../multimedia/click.svg";
 import { useHistory } from "react-router-dom";
 import { eventViewContent } from "../../utils/pixelEvents/PixelEvents";
 const LandingVideo = () => {
@@ -27,53 +28,61 @@ const LandingVideo = () => {
     >
       <Header />
       <div className="text-center max-w-[1200px] mx-auto">
-        <h3
-          className="text-lg md:text-2xl mb-4 font-bold mx-4 my-4 md:my-0"
-          style={{ marginBottom: "1rem" }}
-        >
-          {" "}
-          COMO LOGRAR OBTENER UNA RENTABILIDAD DEL{" "}
-          <span className="underline">10%</span> AL{" "}
-          <span className="underline">30%</span> MENSUAL HACIENDO COPYTRADING
-        </h3>
-        <h1 className="font-open-sans text-3xl md:text-5xl font-extrabold mb-4">
-          PASO 1: MIRA EL VIDEO
-        </h1>
+        <div className="overflow-hidden max-w-[400px] md:max-w-[970px] text-center mt-4 mx-auto">
+          <h1 className=" font-inter text-4xl md:text-5xl font-extrabold text-gray-800 leading-custom mt-4 mx-4 md:mx-0">
+            PASO 1: <span className=" text-custom-blue">MIRA EL VIDEO</span>
+          </h1>
+        </div>
         <div className="mx-auto max-w-[700px] mb-8 mt-8">
           <DropboxVideo />
         </div>
-        <h1 className="font-open-sans text-3xl md:text-5xl font-extrabold mb-4">
-          PASO 2: AGENDÁ UNA REUNION CON NUESTRO EQUIPO
-        </h1>
-        <div className="border-2 border-red-500 mx-4">
-          <div className="mt-2">
-          <h3 className="font-open-sans text-sm md:text-lg font-extrabold text-red-500 mb-4 mx-4 my-4 md:my-0">
-            *Si ya tienes cuenta en Libertex puedes registrarte con el mismo
-            nombre y número de teléfono pero distinto email.
-          </h3>
-          <h3 className="font-open-sans text-sm md:text-lg font-extrabold text-red-500 mb-4 mx-4 my-4 md:my-0">
-            *Solamente regístrate si tienes más de $100 dólares para depositar
-            en tu cuenta de trading
-          </h3>
-          <h3
-            className="font-open-sans text-sm md:text-lg font-extrabold text-red-500 mb-4 mx-4 my-4 md:my-0"
-            style={{ marginBottom: "1rem" }}
+        <div className="overflow-hidden max-w-[350px] md:max-w-[970px] text-center mt-4 mx-auto">
+          <h1 className="font-open-sans text-3xl md:text-5xl font-extrabold mb-4">
+            PASO 2: <span className=" text-custom-blue">AGENDÁ UNA REUNION</span> {" "}CON NUESTRO EQUIPO
+          </h1>
+        </div>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <button
+            className="ov-btn-slide-left max-w-[700px] bg-gradient-to-r from-blue-400 to-blue-700 text-white text-2xl py-4 px-6 rounded-xl mb-2 mx-4 my-0 md:my-2"
+            onClick={() => handleClick()}
+            style={{
+              marginBottom: "3rem",
+              display: "flex",
+              justifyContent: "center",
+            }}
           >
-            *No somos una empresa multinivel ni un fondo de inversión, somos una
-            academia de trading asociada al broker Libertex.
-          </h3>
+            <img
+              src={click}
+              alt="click"
+              style={{
+                width: "31px",
+                height: "31px",
+                marginRight: "5px",
+                fill: "white",
+              }}
+            />
+            AGENDÁ TU REUNIÓN
+          </button>
+        </div>
+        <div className="border-2 border-red-500 mx-4 mb-20 mt-8">
+          <div className="mt-2">
+            <h3 className="font-open-sans text-sm md:text-lg font-extrabold text-red-500 mb-4 mx-4 my-4 md:my-0">
+              *Si ya tienes cuenta en Libertex puedes registrarte con el mismo
+              nombre y número de teléfono pero distinto email.
+            </h3>
+            <h3 className="font-open-sans text-sm md:text-lg font-extrabold text-red-500 mb-4 mx-4 my-4 md:my-0">
+              *Solamente regístrate si tienes más de $100 dólares para depositar
+              en tu cuenta de trading
+            </h3>
+            <h3
+              className="font-open-sans text-sm md:text-lg font-extrabold text-red-500 mb-4 mx-4 my-4 md:my-0"
+              style={{ marginBottom: "1rem" }}
+            >
+              *No somos una empresa multinivel ni un fondo de inversión, somos
+              una academia de trading asociada al broker Libertex.
+            </h3>
           </div>
         </div>
-        <button
-          className="ov-btn-slide-left max-w-[700px] text-white text-2xl py-4 px-6 rounded-lg mb-4 mx-4 my-4 md:my-6"
-          onClick={() => handleClick()}
-        >
-          REGÍSTRATE!
-          <p className="text-0.875rem leading-1.25rem">
-            (sólo para personas que quieren iniciarse en el trading o que ya se
-            iniciaron pero no tienen buenos resultados).
-          </p>
-        </button>
       </div>
       <Footer />
     </div>
