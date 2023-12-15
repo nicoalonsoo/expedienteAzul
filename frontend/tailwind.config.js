630605b5a11fe736c6a45aba6859755da4a6affe
NAlonsoBulacio <nicoalonso99.na@gmail.com>
2023-12-15 16:03:40 -0300

    line-heigth

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -19,7 +19,7 @@ module.exports = withMT({
       },
       lineHeight: {
         '1.25rem': '1.25rem',
-        'custom': '1.77rem',
+        'custom': '1.88rem',
       },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
