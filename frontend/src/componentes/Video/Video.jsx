4f4b358ff3a13bc17a08875d109f78b591ed4c89
NAlonsoBulacio <nicoalonso99.na@gmail.com>
2024-08-07 13:51:36 -0300

    video changed

--- frontend/src/componentes/Video/Video.jsx
+++ frontend/src/componentes/Video/Video.jsx
@@ -19,7 +19,7 @@ function Video() {
 
   return (
     <div className="youtube-div flex justify-center py-4">
-      <iframe ref={videoRef} width="560" height="315" src="https://www.youtube.com/embed/XmH2BZxp_Fg?si=91aUD2FBU2YgkUKz" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      <iframe ref={videoRef} width="560" height="315" src="https://www.youtube.com/embed/972vEKgAp34?si=91aUD2FBU2YgkUKz" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     </div>
   );
 }
